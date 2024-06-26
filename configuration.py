4a39ec210fa89019f958192f567abacfd87dcd07
MacBook Pro <gcastellanosvillanueva@gmail.com>
2024-06-25 21:56:34 -0500

    my first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-dde0a2d9-caeb-4b3e-8904-d00f9ec3ce22.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
